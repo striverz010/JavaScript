e4540245805654def0ef83ade42d613aa5cdfe87
striver011 <striveredu011@gmail.com>
2023-09-25 12:22:48 +0530

    primitive types❤️

---
+++ PrimitiveTypes/script.js
@@ -0,0 +1,16 @@
+
+let number=143;
+
+let name1="Manikanta Korimilli";
+
+let Approve=true;
+
+let firstName=undefined;
+
+let lastName=null;
+
+console.log(number, typeof number); //143  number
+console.log(name1, typeof name1);  //manikanta korimilli  name
+console.log(Approve, typeof Approve); //true  Boolean
+console.log(firstName,typeof firstName); //undefind undefind
+console.log(lastName, typeof lastName); //null object
